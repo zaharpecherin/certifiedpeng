d03e95ab3f77fa5fce23b302fa8b1b2c53473976
Zahar Pecherin <zaharpecherin88@gmail.com>
2017-03-27 09:34:24 +0300

    Some changes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -8,7 +8,7 @@ class SiteController < ApplicationController
 
   def dashboard
     @top_likes = Like.get_top_likes
-    @user_tags = current_user.tags.pluck(:tag_name)
+    @user_tags = current_user.tags.pluck(:tag_name).join(', ')
 
     @not_added_tags = Tag::MAX_TAGS_COUNT - current_user.tags.count
     query = params[:search] if params[:search]
