01f347e56942cd5b768fe35b107f2a27c2e87c8c
Zahar Pecherin <zaharpecherin88@gmail.com>
2017-04-06 15:31:53 +0300

    Added redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  APP_DOMAIN = 'www.certifiedpeng.com'
+  before_filter :ensure_domain
+
+  def ensure_domain
+    unless request.env['HTTP_HOST'] == APP_DOMAIN || Rails.env.development?
+      redirect_to "http://#{APP_DOMAIN}", status: 301
+    end
+  end
 end
