fa55dac2d4dae3bc18485942d232af8ab7737ebf
Zahar Pecherin <zaharpecherin88@gmail.com>
2020-02-06 13:58:53 +0300

    Added cookies popup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,17 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery.cookieMessage
 //= require_tree .
 
 $(document).on('turbolinks:load', function(){
     setTimeout(function(){
         $( ".stripe-button-el" ).prop( "disabled", true );
     }, 500);
+
+    $.cookieMessage({
+        'mainMessage':'This website uses cookies. By using this website you consent to our use of these cookies. For more information visit our <a href="https://www.certifiedpeng.com/terms#cookies">Privacy Policy</a>. '
+    });
 });
 
 
